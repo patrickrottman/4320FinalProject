335445d7e71dfbfc01ce98d1b5f336601feb2059
Rottman, Patrick J. (MU-Student) <pjrc74@mail.missouri.edu>
2018-04-27 15:35:11 -0500

    finished comparing feature and added output to screen.

--- GradeChecker/GradeChecker/MainWindow.xaml.cs
+++ GradeChecker/GradeChecker/MainWindow.xaml.cs
@@ -46,7 +46,8 @@ namespace GradeChecker
 
         private void startUnzip_Click(object sender, RoutedEventArgs e)
         {
-            if((ZipFilesLocation != null && ZipFilesLocation != "") || ExportFolderLocation != null && ExportFolderLocation != "")
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait; // set the cursor to loading spinner
+            if ((ZipFilesLocation != null && ZipFilesLocation != "") || ExportFolderLocation != null && ExportFolderLocation != "")
             {
                 ZipFile.ExtractToDirectory(ZipFilesLocation, ExportFolderLocation);
                 DirectoryInfo d = new DirectoryInfo(ExportFolderLocation);
@@ -69,8 +70,10 @@ namespace GradeChecker
             else
             {
                 System.Windows.MessageBox.Show("Please choose file directory first.");
+                Mouse.OverrideCursor = System.Windows.Input.Cursors.Arrow; // set the cursor back to arrow
                 return;
             }
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Arrow; // set the cursor back to arrow
         }
 
         private void FileExportButton_Click(object sender, RoutedEventArgs e)
@@ -85,7 +88,24 @@ namespace GradeChecker
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait; // set the cursor to loading spinner
             WalkDirectoryTree(new DirectoryInfo(ExportFolderLocation));
+
+            foreach(StudentFile student in studentFileList)
+            {
+                foreach(StudentFile innerStudent in studentFileList)
+                {
+                    if(student.fileName != innerStudent.fileName)
+                    {
+                        double similarity = SimilarityinPercentage(student.fileContents, innerStudent.fileContents);
+                        if(similarity > 60)
+                        {
+                            SimilarStudentFileNames.Items.Add(student.fileName + " and " + innerStudent.fileName + " are " + similarity.ToString("0.00") + "% similar.");
+                        }
+                    }
+                }
+            }
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Arrow; // set the cursor back to arrow
         }
 
         private void WalkDirectoryTree(DirectoryInfo dr)
@@ -99,6 +119,7 @@ namespace GradeChecker
                     studentFileName = file.FullName.Replace(ExportFolderLocation, "");
                     studentFileName = studentFileName.Substring(1, studentFileName.Length-1);
                     studentFileName = studentFileName.Substring(0, studentFileName.IndexOf(@"\"));
+                    studentFileName = studentFileName.Substring(0, studentFileName.IndexOf("_"));
                     //file.Name.Substring(0, file.Name.Length - 4);
 
 
@@ -115,15 +136,42 @@ namespace GradeChecker
             return startDirectory.EnumerateFiles(pattern, SearchOption.AllDirectories);
         }
 
-        public void SmilarityinPercentage(String original, String comparison)
+        public double SimilarityinPercentage(String original, String comparison)
         {
+            original = original.Replace("\r", "");
+            comparison = comparison.Replace("\r", "");
+
+            original = original.Replace("\"", "");
+            comparison = comparison.Replace("\"", "");
+
+            original = original.Replace("{", "");
+            comparison = comparison.Replace("{", "");
+
+            original = original.Replace("}", "");
+            comparison = comparison.Replace("}", "");
+
+
+            
+            IQueryable<String> splitString1 = original.Split('\n').AsQueryable();
+            IQueryable<String> splitString2 = comparison.Split('\n').AsQueryable();
+
+            splitString1 = splitString1.Where(s => !string.IsNullOrWhiteSpace(s)).AsQueryable();
+            splitString2 = splitString2.Where(s => !string.IsNullOrWhiteSpace(s)).AsQueryable();
+
+            splitString1 = splitString1.Where(x => !x.Contains("using")).AsQueryable();
+            splitString2 = splitString2.Where(x => !x.Contains("using")).AsQueryable();
+
+            splitString1 = splitString1.Where(x => !x.Contains("///")).AsQueryable();
+            splitString2 = splitString2.Where(x => !x.Contains("///")).AsQueryable();
+
+
+
             // code from http://www.dotnetworld.in/2013/05/c-find-similarity-between-two-strings.html
-            string[] splitString1 = original.Split(' ');
-            string[] splitString2 = comparison.Split(' ');
             var strCommon = splitString1.Intersect(splitString2);
             //Formula : Similarity (%) = 100 * (CommonItems * 2) / (Length of String1 + Length of String2)
-            double Similarity = (double)(100 * (strCommon.Count() * 2)) / (splitString1.Length + splitString2.Length);
+            double Similarity = (double)(100 * (strCommon.Count() * 2)) / (splitString1.Count() + splitString2.Count());
             Console.WriteLine("Strings are {0}% similar", Similarity.ToString("0.00"));
+            return Similarity;
         }
     }
 }
